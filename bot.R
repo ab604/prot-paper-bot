9509aadf5c0c24cb91e3e51d2cbed2c38cf2993d
ab604 <ab604@soton.ac.uk>
2024-09-10 17:49:46 +0100

    restore feeds

--- bot.R
+++ bot.R
@@ -1,5 +1,5 @@
 # Bot script adapted from https://github.com/JBGruber/r-bloggers-bluesky bot.r
-# A.Bailey 2024-09-06
+# A.Bailey 2024-09-10
 
 ## packages
 library(tidyRSS)
@@ -20,8 +20,7 @@ library(xml2)
 # immunopeptidom*[tiab] AND neoantigen*[tiab]
 pubmed_feeds <- c("https://pubmed.ncbi.nlm.nih.gov/rss/search/1jsI3JGQCWWBHeHK4cUErWUE19BvzlvyZfNdMmdXcysd7rmgww/?limit=15&utm_campaign=pubmed-2&fc=20240902141654",
                   "https://pubmed.ncbi.nlm.nih.gov/rss/search/1RKSf0HH9l2s1BIME29OLF8W10zHSLgJVuDXmjq8ihvd8F3Aro/?limit=100&utm_campaign=pubmed-2&fc=20240902141801",
-                  "https://pubmed.ncbi.nlm.nih.gov/rss/search/1RKSf0HH9l2s1BIME29OLF8W10zHSLgJVuDXmjq8ihvd8F3Aro/?limit=100&utm_campaign=pubmed-2&fc=20240902141801",
-                  "https://pubmed.ncbi.nlm.nih.gov/rss/search/1hEma6JdH30oOMMmBp0-KmZH8miGfDm3XJPkSNxx4xT_wg2sgf/?limit=5&utm_campaign=pubmed-2&fc=20240910121226")
+                  "https://pubmed.ncbi.nlm.nih.gov/rss/search/1RKSf0HH9l2s1BIME29OLF8W10zHSLgJVuDXmjq8ihvd8F3Aro/?limit=100&utm_campaign=pubmed-2&fc=20240902141801")
 
 # Read all the PubMed feeds
 pubmed_df <- map_df(pubmed_feeds, tidyfeed) 
@@ -55,7 +54,7 @@ brv_filt <- brv |>
 
 # Filter for Pubmed feed for keywords and publication of no earlier than last 30 days and trim link
 pubmed_filt <- pubmed_df |> 
-  filter(str_detect(item_title, "[Ii]mmunopep*|[Pp]eptidomi*|[Pp]eptidome|HDX-MS|([Pp]roteogenomics & [Nn]eoantigen)|overabundance") |
+  filter(str_detect(item_title, "[Ii]mmunopep*|[Pp]eptidomi*|[Pp]eptidome|HDX-MS|([Pp]roteogenomics & [Nn]eoantigen)") |
            str_detect(item_description, "[Ii]mmunopep*|[Pp]eptidomi*|[Pp]eptidome|HDX-MS|([Pp]roteogenomics & [Nn]eoantigen)"),
          item_pub_date >= today() - 29) |> 
   mutate(link = str_extract(item_link,"^.*?[^?]*"))
@@ -85,7 +84,7 @@ old_posts <- get_skeets_authored_by("protpapers.bsky.social", limit = 5000L)
 posts_new <- posts |>
   filter(!post_text %in% old_posts$text)
 
-## Part 4: Post skeets!
+## Part 4: Post skeets. preview_card = FALSE means no images.
 for (i in seq_len(nrow(posts_new))) {
   # if people upload broken preview images, this fails
   resp <- try(post_skeet(text = posts_new$post_text[i],
